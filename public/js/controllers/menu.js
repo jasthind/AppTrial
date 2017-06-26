804ca60537c9ac4f88f332a18671ef590ad54471
Monisha Gunalan2 <moni.sind@gmail.com>
2017-06-25 22:31:18 -0400

    Redesigned menu page

--- public/js/controllers/menu.js
+++ public/js/controllers/menu.js
@@ -3,14 +3,70 @@ angular
     .module('app')
     .controller('menuController', menuController);
 
-menuController.$inject = ['$scope','loginService'];
+menuController.$inject = ['$scope','loginService', '$state'];
 
 function menuController($scope, loginService, $state) {
     $scope.dishes = [
-        { name: 'Butter Chicken', days: ['Mon', 'Tues'] },
-        { name: 'Chicken Tikka', days: ['Wed', 'Thurs'] },
-        { name: 'Tandoori Chicken', days: ['Fri'] },
-        { name: 'Paneer Masala', days: ['Mon', 'Wed'] }
+        {
+            name: 'Butter Chicken',
+            image:'img/menu/butterChicken.jpg',
+            days: ['Mon', 'Tues']
+        },
+        {
+            name: 'Chicken Tikka',
+            image:'img/menu/chickenTikka.jpg',
+            days: ['Wed', 'Thurs']
+        },
+        {
+            name: 'Tandoori Chicken',
+            image:'img/menu/tandooriChicken.jpg',
+            days: ['Fri']
+        },
+        {
+            name: 'Paneer Butter Masala',
+            image:'img/menu/paneerButterMasala.jpg',
+            days: ['Mon', 'Wed']
+        }
     ];
+
+    $scope.days = [
+        {
+            name: 'Monday',
+            dishes: [
+                {name:'Butter Chicken', image: 'img/menu/butterChicken.jpg'},
+                {name:'Paneer Butter Masala', image: 'img/menu/paneerButterMasala.jpg'}
+            ]
+        },
+        {
+            name: 'Tuesday',
+            dishes: [
+                {name:'Butter Chicken', image: 'img/menu/butterChicken.jpg'}
+            ]
+        },
+        {
+            name: 'Wednesday',
+            dishes: [
+                {name:'Chicken Tikka', image: 'img/menu/chickenTikka.jpg'},
+                {name:'Paneer Butter Masala', image: 'img/menu/paneerButterMasala.jpg'}
+            ]
+        },
+        {
+            name: 'Thursday',
+            dishes: [
+                {name:'Chicken Tikka', image: 'img/menu/chickenTikka.jpg'}
+            ]
+        },
+        {
+            name: 'Friday',
+            dishes: [
+                {name:'Tandoori Chicken', image: 'img/menu/tandooriChicken.jpg'}
+            ]
+        }
+    ]
+
+
+    $scope.selectDish = function selectDish() {
+        $state.go('order');
+    };
 }
 
